355afff208e54e570b378e9a09e92e779bcfc121
Kornilin5 <sasha_kornilin@mail.ru>
2024-02-20 13:32:51 +0400

    добавил изменения в конфтест

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from qa_guru_python_12.utils import attach
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser(request):
-    browser.config.base_url = "https://demoqa.com"
+    browser.config.base_url = "https://demoqa.com/automation-practice-form"
     browser.config.window_width = 1920
     browser.config.window_height = 1080
 
